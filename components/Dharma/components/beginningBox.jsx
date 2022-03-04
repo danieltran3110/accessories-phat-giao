9f8af4d9a13bca1437cafe660a2f80069f43e639
TheHien102 <huynhthehien11102001@gmail.com>
2022-03-04 22:16:20 +0700

    fix bug responsive and fonts

--- components/Dharma/components/beginningBox.jsx
+++ components/Dharma/components/beginningBox.jsx
@@ -5,12 +5,10 @@ import stylesGlobal from '../../../assets/scss/global.module.scss';
 
 const BeginningBox = () => {
   return (
-    <div className={stylesGlobal.container2}>
       <div className={styles.beginningBox}>
         <p className={styles.content}>{dataBeginningBox.content}</p>
         <p className={styles.btnStart}>{dataBeginningBox.btnName}</p>
       </div>
-    </div>
   );
 };
 
