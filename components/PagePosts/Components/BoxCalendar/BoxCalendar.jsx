b9dba91869d2b90dfcca806dfefa024624fdc607
TheHien102 <huynhthehien11102001@gmail.com>
2022-04-07 23:14:18 +0700

    update calendar box in page posts

--- components/PagePosts/Components/BoxCalendar/BoxCalendar.jsx
+++ components/PagePosts/Components/BoxCalendar/BoxCalendar.jsx
@@ -4,11 +4,11 @@ import Calendar from 'react-calendar';
 import stylesGlobal from '../../../../assets/scss/global.module.scss';
 
 const BoxCalendar = () => {
-  const [value, onChange] = useState(new Date());
+  // const [value, onChange] = useState(new Date());
 
   return (
     <div className={stylesGlobal.backgroundColor}>
-      <Calendar className={styles.calendar} onChange={onChange} value={value} />
+      <Calendar className={styles.calendar} />
     </div>
   );
 };
