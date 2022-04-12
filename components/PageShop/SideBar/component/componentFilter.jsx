cf8b4da09470ec3097126941e1b39f17c5fe7c86
TheHien102 <huynhthehien11102001@gmail.com>
2022-04-12 16:53:41 +0700

    adding styles filter in page shop

--- components/PageShop/SideBar/component/componentFilter.jsx
+++ components/PageShop/SideBar/component/componentFilter.jsx
@@ -16,7 +16,7 @@ const Filter = () => {
     <div className={stylesGlobal.backgroundColor}>
       <div className={styles.wrapperBox}>
         <h2 className={styles.title}>Filter by price</h2>
-        <Box sx={{ width:' 100% '}}>
+        <Box sx={{ width: ' 100% ' }}>
           <Slider
             className={styles.sliderPrice}
             value={value}
@@ -25,9 +25,47 @@ const Filter = () => {
             min={8}
             max={15}
             // defaultValue={8}
+            sx={{
+              '& .css-187mznn-MuiSlider-root': {
+                marginLeft: '10px',
+                paddingRight: '10px',
+                cursor: 'e-resize',
+              },
+              '& .css-14pt78w-MuiSlider-rail': {
+                display: 'none',
+                cursor: 'e-resize',
+              },
+
+              '& .MuiSlider-mark': {
+                display: 'none',
+              },
+
+              '& .css-eg0mwd-MuiSlider-thumb': {
+                borderRadius: 0,
+                width: '15px',
+                height: '15px',
+                cursor: 'e-resize',
+
+                '&:after': {
+                  display: 'none',
+                },
+                '&:hover': {
+                  boxShadow: 'none',
+
+                },
+              },
+
+              '& .Mui-active': {
+                boxShadow: 'none',
+              },
+              '& .Mui-focusVisible': {
+                boxShadow: 'none',
+              },
+            }
+            }
           />
         </Box>
-        <p className={styles.price}>PRICE: <span>${value[0]} - ${value[1]}</span> </p>
+        <p className={styles.price}>PRICE: <span>${value[0]} - ${value[1]}</span></p>
         <p className={styles.btnFilter}>FILTER</p>
       </div>
     </div>
