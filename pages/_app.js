40e7960d8ee622b21e4dedb6a4f7eb8052cb2aed
TheHien102 <huynhthehien11102001@gmail.com>
2022-05-03 20:35:47 +0700

    fix bug change name github

--- pages/_app.js
+++ pages/_app.js
@@ -52,19 +52,6 @@ function MyApp({ Component, pageProps }) {
         crossOrigin='anonymous'
       />
       <Script async src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
-      {/* eslint-disable-next-line @next/next/inline-script-id */}
-      <Script dangerouslySetInnerHTML={{
-        __html: `
-              if (window.netlifyIdentity) {
-                window.netlifyIdentity.on("init", user => {
-                  if (!user) {
-                    window.netlifyIdentity.on("login", () => {
-                      document.location.href = "/admin/";
-                    });
-                  }
-                });
-              }
-          `}}/>
       <Component {...pageProps} />
     </>
   );
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { StyleSheetServer } from 'aphrodite';
 import React from 'react';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   static async getInitialProps({ renderPage }) {
@@ -36,6 +37,18 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `}}/>
         </body>
       </Html>
     );
