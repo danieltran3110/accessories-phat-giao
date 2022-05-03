2b6dd6710a0eafa977d234bbf236acd36a3510a4
TheHien102 <huynhthehien11102001@gmail.com>
2022-05-03 21:44:38 +0700

    change nextjs config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  webpack: (cfg, { isServer }) => {
+    cfg.module.rules.push({
+                            test: /\.md$/,
+                            loader: 'frontmatter-markdown-loader',
+                            options: { mode: ['react-component'] },
+                          });
+    if (!isServer) {
+      cfg.resolve.fallback = {
+        fs: false,
+      };
+    }
+    return cfg;
+  },
 };
